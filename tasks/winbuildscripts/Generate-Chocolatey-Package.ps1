1e58e852414e7daaf7972b7fca6e251266a111f3
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-07-01 14:43:46 +0200

    Install python deps where needed

--- tasks/winbuildscripts/Generate-Chocolatey-Package.ps1
+++ tasks/winbuildscripts/Generate-Chocolatey-Package.ps1
@@ -7,6 +7,10 @@ Param(
 
 $ErrorActionPreference = 'Stop';
 Set-Location c:\mnt
+
+# Install python deps
+python3 -m pip install -r requirements.txt
+
 $outputDirectory = "c:\mnt\build-out"
 $rawAgentVersion = (inv agent.version)
 $copyright = "Datadog {0}" -f (Get-Date).Year
