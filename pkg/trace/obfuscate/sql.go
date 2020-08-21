cf3f70c63cd85cdfbf7a11918a8d35d9a0ddf11f
Justin Iso <justin.james.iso@gmail.com>
2020-08-21 16:39:41 -0400

    Revert "Modify SQL obfuscation to normalize bind parameters, join tokens separated by periods, and support additional tokens in different dialects"
    This reverts commit 848fe0bc52750f0151e8ef6082391cab96de1dee.

--- pkg/trace/obfuscate/sql.go
+++ pkg/trace/obfuscate/sql.go
@@ -54,15 +54,13 @@ func (f *discardFilter) Filter(token, lastToken TokenKind, buffer []byte) (Token
 	switch lastToken {
 	case FilteredBracketedIdentifier:
 		if token != ']' {
-			switch token {
-			case ID, '.':
-				// we haven't found the closing bracket yet, keep going
-				return FilteredBracketedIdentifier, nil, nil
-			default:
-				// the token between the brackets *must* be a whitelisted identifier,
+			// we haven't found the closing bracket yet, keep going
+			if token != ID {
+				// the token between the brackets *must* be an identifier,
 				// otherwise the query is invalid.
 				return LexError, nil, fmt.Errorf("expected identifier in bracketed filter, got %d", token)
 			}
+			return FilteredBracketedIdentifier, nil, nil
 		}
 		fallthrough
 	case As:
@@ -107,10 +105,6 @@ func (f *replaceFilter) Filter(token, lastToken TokenKind, buffer []byte) (token
 		}
 	}
 	switch token {
-	case BindParameterAt:
-		return token, []byte("@?"), nil
-	case BindParameterColon:
-		return token, []byte(":?"), nil
 	case String, Number, Null, Variable, PreparedStatement, BooleanLiteral, EscapeSequence:
 		return FilteredGroupable, []byte("?"), nil
 	default:
@@ -289,7 +283,7 @@ func attemptObfuscation(tokenizer *SQLTokenizer) (*ObfuscatedQuery, error) {
 		if buff != nil {
 			if out.Len() != 0 {
 				switch token {
-				case ',', '.':
+				case ',':
 				case '=':
 					if lastToken == ':' {
 						// do not add a space before an equals if a colon was
@@ -298,11 +292,7 @@ func attemptObfuscation(tokenizer *SQLTokenizer) (*ObfuscatedQuery, error) {
 					}
 					fallthrough
 				default:
-					switch lastToken {
-					case '.', '@':
-					default:
-						out.WriteRune(' ')
-					}
+					out.WriteRune(' ')
 				}
 			}
 			out.Write(buff)
