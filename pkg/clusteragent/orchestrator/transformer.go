863684d65b17873764a07128a2e3693aaafa6daf
nam <nam.nguyen@datadoghq.com>
2020-08-28 17:23:28 +0200

    [orchestrator] initial node agent
    add reno
    
    initial convert node work
    
    add transformer
    
    update gomod and adding converting code
    
    add missing cache sync
    
    fmt file
    
    update payload and transformer with test
    
    move import
    
    update go mod
    
    fix test for payload
    
    go fmt
    
    fix NPE for transformer
    
    fix fmt
    
    add exp int
    
    address part feedback for redundant checks
    
    address log feedback
    
    fix unit tests
    
    add additional tags for nodes

--- pkg/clusteragent/orchestrator/transformer.go
+++ pkg/clusteragent/orchestrator/transformer.go
@@ -8,12 +8,15 @@
 package orchestrator
 
 import (
+	"strings"
+
 	model "github.com/DataDog/agent-payload/process"
 	"github.com/DataDog/datadog-agent/pkg/process/util/orchestrator"
 
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 func extractDeployment(d *v1.Deployment) *model.Deployment {
@@ -193,4 +196,174 @@ func extractDeploymentConditionMessage(conditions []v1.DeploymentCondition) stri
 		}
 	}
 	return ""
+}
+
+func extractNode(n *corev1.Node) *model.Node {
+	msg := &model.Node{
+		Metadata:      orchestrator.ExtractMetadata(&n.ObjectMeta),
+		PodCIDR:       n.Spec.PodCIDR,
+		PodCIDRs:      n.Spec.PodCIDRs,
+		ProviderID:    n.Spec.ProviderID,
+		Unschedulable: n.Spec.Unschedulable,
+		Status: &model.NodeStatus{
+			Allocatable: map[string]int64{},
+			Capacity:    map[string]int64{},
+		},
+	}
+
+	if len(n.Spec.Taints) > 0 {
+		msg.Taints = extractTaints(n.Spec.Taints)
+	}
+
+	// status
+	msg.Status.KubeletVersion = n.Status.NodeInfo.KubeletVersion
+	msg.Status.ContainerRuntimeVersion = n.Status.NodeInfo.ContainerRuntimeVersion
+	msg.Status.OperatingSystem = n.Status.NodeInfo.OperatingSystem
+	msg.Status.Architecture = n.Status.NodeInfo.Architecture
+	msg.Status.OsImage = n.Status.NodeInfo.OSImage
+	msg.Status.KubeProxyVersion = n.Status.NodeInfo.KubeProxyVersion
+	msg.Status.KernelVersion = n.Status.NodeInfo.KernelVersion
+	extractCapacitiesAndAllocatables(n, msg)
+
+	// extract status addresses
+	if len(n.Status.Addresses) > 0 {
+		msg.Status.NodeAddresses = map[string]string{}
+		for _, address := range n.Status.Addresses {
+			msg.Status.NodeAddresses[string(address.Type)] = address.Address
+		}
+	}
+
+	// extract conditions
+	if len(n.Status.Conditions) > 0 {
+		for _, condition := range n.Status.Conditions {
+			c := &model.NodeCondition{
+				Type:    string(condition.Type),
+				Status:  string(condition.Status),
+				Reason:  condition.Reason,
+				Message: condition.Message,
+			}
+			if !condition.LastTransitionTime.IsZero() {
+				c.LastTransitionTime = condition.LastTransitionTime.Unix()
+			}
+			msg.Status.Conditions = append(msg.Status.Conditions, c)
+		}
+	}
+
+	// extract status message
+	msg.Status.Status = computeNodeStatus(n)
+
+	// extract role
+	roles := findNodeRoles(n)
+	if len(roles) > 0 {
+		msg.Roles = roles
+	}
+
+	for _, image := range n.Status.Images {
+		msg.Status.Images = append(msg.Status.Images, &model.ContainerImage{
+			Names:     image.Names,
+			SizeBytes: image.SizeBytes,
+		})
+	}
+
+	return msg
+}
+
+func extractCapacitiesAndAllocatables(n *corev1.Node, mn *model.Node) {
+	// Milli Value ceil(q * 1000), which fits to be the lowest value. CPU -> Millicore and Memory -> byte
+	supportedResourcesMilli := []corev1.ResourceName{corev1.ResourceCPU, corev1.ResourceMemory}
+	supportedResources := []corev1.ResourceName{corev1.ResourcePods}
+	setSupportedResources(n, mn, supportedResources, false)
+	setSupportedResources(n, mn, supportedResourcesMilli, true)
+}
+
+func setSupportedResources(n *corev1.Node, mn *model.Node, supportedResources []corev1.ResourceName, isMilli bool) {
+	for _, resource := range supportedResources {
+		capacity, hasCapacity := n.Status.Capacity[resource]
+		if hasCapacity && !capacity.IsZero() {
+			if isMilli {
+				mn.Status.Capacity[resource.String()] = capacity.MilliValue()
+			} else {
+				mn.Status.Capacity[resource.String()] = capacity.Value()
+			}
+		}
+		allocatable, hasAllocatable := n.Status.Allocatable[resource]
+
+		if hasAllocatable && !allocatable.IsZero() {
+			if isMilli {
+				mn.Status.Allocatable[resource.String()] = allocatable.MilliValue()
+			} else {
+				mn.Status.Allocatable[resource.String()] = allocatable.Value()
+			}
+		}
+	}
+}
+
+func extractTaints(taints []corev1.Taint) []*model.Taint {
+	modelTaints := make([]*model.Taint, 0, len(taints))
+
+	for _, taint := range taints {
+		modelTaint := &model.Taint{
+			Key:    taint.Key,
+			Value:  taint.Value,
+			Effect: string(taint.Effect),
+		}
+		if taint.TimeAdded != nil && !taint.TimeAdded.IsZero() {
+			modelTaint.TimeAdded = taint.TimeAdded.Unix()
+		}
+		modelTaints = append(modelTaints, modelTaint)
+	}
+	return modelTaints
+}
+
+// computeNodeStatus is mostly copied from kubernetes to match what users see in kubectl
+// in case of issues, check for changes upstream: https://github.com/kubernetes/kubernetes/blob/1e12d92a5179dbfeb455c79dbf9120c8536e5f9c/pkg/printers/internalversion/printers.go#L1410
+func computeNodeStatus(n *corev1.Node) string {
+	conditionMap := make(map[corev1.NodeConditionType]*corev1.NodeCondition)
+	NodeAllConditions := []corev1.NodeConditionType{corev1.NodeReady}
+	for i := range n.Status.Conditions {
+		cond := n.Status.Conditions[i]
+		conditionMap[cond.Type] = &cond
+	}
+	var status []string
+	for _, validCondition := range NodeAllConditions {
+		if condition, ok := conditionMap[validCondition]; ok {
+			if condition.Status == corev1.ConditionTrue {
+				status = append(status, string(condition.Type))
+			} else {
+				status = append(status, "Not"+string(condition.Type))
+			}
+		}
+	}
+	if len(status) == 0 {
+		status = append(status, "Unknown")
+	}
+	if n.Spec.Unschedulable {
+		status = append(status, "SchedulingDisabled")
+	}
+	return strings.Join(status, ",")
+
+}
+
+// findNodeRoles returns the roles of a given node.
+// The roles are determined by looking for:
+// * a node-role.kubernetes.io/<role>="" label
+// * a kubernetes.io/role="<role>" label
+// is mostly copied from kubernetes, for issues check upstream: https://github.com/kubernetes/kubernetes/blob/1e12d92a5179dbfeb455c79dbf9120c8536e5f9c/pkg/printers/internalversion/printers.go#L1487
+func findNodeRoles(node *corev1.Node) []string {
+	labelNodeRolePrefix := "node-role.kubernetes.io/"
+	nodeLabelRole := "kubernetes.io/role"
+
+	roles := sets.NewString()
+	for k, v := range node.Labels {
+		switch {
+		case strings.HasPrefix(k, labelNodeRolePrefix):
+			if role := strings.TrimPrefix(k, labelNodeRolePrefix); len(role) > 0 {
+				roles.Insert(role)
+			}
+
+		case k == nodeLabelRole && v != "":
+			roles.Insert(v)
+		}
+	}
+	return roles.List()
 }
